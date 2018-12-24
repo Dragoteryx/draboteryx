067e7440beec1715df0a0412c0bf82a58305d08c
Dragoteryx <33252782+Dragoteryx@users.noreply.github.com>
2018-12-24 21:19:25 +0100

    cleverbot fix

--- src/cleverbot.js
+++ src/cleverbot.js
@@ -2,19 +2,23 @@ const Cleverbot = require("cleverbot.io");
 const clever = new Cleverbot(process.env.CLEVER_USER, process.env.CLEVER_KEY);
 module.exports = msg => {
   return new Promise(async (resolve, reject) => {
-    if (msg.author.cleverResponsing) return null;
-    msg.author.cleverResponsing = true;
+    if (msg.author.cleverResponding) return null;
+    msg.author.cleverResponding = true;
     clever.setNick(msg.channel.id);
     clever.create((err, session) => {
       if (err) {
-        msg.author.cleverResponsing = false;
+        msg.author.cleverResponding = false;
         reject(err);
       } else {
-        clever.ask(msg.content, (err, res) => {
-          msg.author.cleverResponsing = false;
-          if (err) reject(err);
-          else resolve(res);
-        });
+        try {
+          clever.ask(msg.content, (err, res) => {
+            msg.author.cleverResponding = false;
+            if (err) reject(err);
+            else resolve(res);
+          });
+        } catch(err) {
+          reject(err);
+        }        
       }
     })
   });
